06fc87c5758c965229187428229fed588df9b233
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-11-22 22:43:35 -0500

    Done

--- zeevm/src/main/java/rocks/zipcode/ZeeOp.java
+++ zeevm/src/main/java/rocks/zipcode/ZeeOp.java
@@ -46,6 +46,23 @@ public enum ZeeOp {
             operandStack.push(operandStack.pop() - second);
         }
     },
+    MULTIPLY("multiply"){
+        public void execute(String[] args) {
+            operandStack.push(operandStack.pop() * operandStack.pop());
+        }
+    },
+    DIVIDE("divide"){
+        public void execute(String[] args) {
+            int second = operandStack.pop();
+            operandStack.push(operandStack.pop() / second);
+        }
+    },
+    MOD("mod"){
+        public void execute(String[] args) {
+            int second = operandStack.pop();
+            operandStack.push(operandStack.pop() % second);
+        }
+    },
     PRINT("print"){
         public void execute(String[] args) {
                 String p = operandStack.pop().toString();
